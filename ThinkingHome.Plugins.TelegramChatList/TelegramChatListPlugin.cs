b87486dd6ec73971e1c86993ae4a875139ad1107
postcretinism <postcretinism@gmail.com>
2025-06-12 23:36:49 +0300

    Создали новый плагин TelegramChatList

---
+++ ThinkingHome.Plugins.TelegramChatList/TelegramChatListPlugin.cs
@@ -0,0 +1,16 @@
+﻿using ThinkingHome.Core.Plugins;
+using ThinkingHome.Plugins.TelegramBot;
+
+namespace ThinkingHome.Plugins.TelegramChatList;
+
+public class TelegramChatListPlugin(TelegramBotPlugin telegramBot) : PluginBase {
+    public override void InitPlugin()
+    {
+        
+    }
+
+    public override void StartPlugin()
+    {
+        telegramBot.SendMessage(353206782, "чатлисту весело он работает !");
+    }
+}
