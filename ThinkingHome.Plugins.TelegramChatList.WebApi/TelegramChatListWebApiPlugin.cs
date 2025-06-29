02278f72760d83e72409d4142da569cfe62599e0
postcretinism <postcretinism@gmail.com>
2025-06-29 13:25:25 +0300

    создали и подключили новый плагин к консоли

---
+++ ThinkingHome.Plugins.TelegramChatList.WebApi/TelegramChatListWebApiPlugin.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+using ThinkingHome.Core.Plugins;
+
+namespace ThinkingHome.Plugins.TelegramChatList.WebApi;
+
+public class TelegramChatListWebApiPlugin() : PluginBase {
+    public override void InitPlugin()
+    {
+    }
+
+    public override void StartPlugin()
+    {
+        Logger.LogInformation("юхуу плагин апишки чат листа работаееееет ееееееее");
+    }
+
+    public override void StopPlugin()
+    {
+    }
+}
